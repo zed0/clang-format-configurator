3efa91d3f636886fd15adc69e2f0377dfebfea56
Ben Falconer <ben@falconers.me.uk>
2015-11-06 22:37:45 +0000

    feat: Add non-standard urls to setup script
    closes #4

--- server/js/settings.js
+++ server/js/settings.js
@@ -1,4 +1,4 @@
 (function(exports){
-	exports.versions = ['3.7.0', '3.6.2', '3.6.1', '3.6.0', '3.5.2', '3.5.0'];
+	exports.versions = ['3.7.0', '3.6.2', '3.6.1', '3.6.0', '3.5.2', '3.5.0', '3.4.2', '3.4.1', '3.4'];
 	exports.port     = 8038;
 })(typeof exports === 'undefined'? this['clang_format_config']={}: exports);
--- setup.sh
+++ setup.sh
@@ -3,6 +3,19 @@ set -e
 
 clang_versions="3.7.0 3.6.2 3.6.1 3.6.0 3.5.2 3.5.0"
 
+clang_other_versions="
+3.4.2,http://llvm.org/releases/3.4.2/cfe-3.4.2.src.tar.gz,http://llvm.org/releases/3.4.2/clang+llvm-3.4.2-x86_64-linux-gnu-ubuntu-14.04.xz
+3.4.1,http://llvm.org/releases/3.4.1/cfe-3.4.1.src.tar.gz,http://llvm.org/releases/3.4.1/clang+llvm-3.4.1-x86_64-unknown-ubuntu12.04.tar.xz
+3.4,http://llvm.org/releases/3.4/clang-3.4.src.tar.gz,http://llvm.org/releases/3.4/clang+llvm-3.4-x86_64-unknown-ubuntu12.04.tar.xz
+"
+
+function tar_flags {
+	echo xv$(echo $1 | sed '
+		/\.xz$/c\J
+		/\.gz$/c\z
+	')
+}
+
 function generate_source_url {
 	echo "http://llvm.org/releases/$1/cfe-$1.src.tar.xz"
 }
@@ -10,6 +23,7 @@ function generate_source_url {
 function generate_binary_url {
 	echo "http://llvm.org/releases/$1/clang+llvm-$1-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
 }
+
 
 cd $( dirname "${BASH_SOURCE[0]}" )
 
@@ -19,16 +33,34 @@ cd ..
 
 cd server/js
 npm install
-cd ..
+cd ../..
 
 cd server/llvm
-for version in $clang_versions
-do
-	source_url=$(generate_source_url $version)
-	mkdir $version.src
-	wget $source_url --quiet -O - | tar xvJ --strip-components=1 -C $version.src --occurrence=1 --wildcards '*/docs/ClangFormatStyleOptions.rst'
 
-	binary_url=$(generate_binary_url $version)
-	mkdir $version
-	wget $binary_url --quiet -O - | tar xvJ --strip-components=1 -C $version --occurrence=1 --wildcards '*bin/clang-format'
+for normal_version in $clang_versions
+do
+	clang_other_versions="$clang_other_versions$normal_version,$(generate_source_url $normal_version),$(generate_binary_url $normal_version)
+"
+done
+
+for tuple in $clang_other_versions
+do
+	IFS=","
+	set $tuple
+	version=$1
+	source_url=$2
+	binary_url=$3
+	if [ ! -d $version.src ]
+	then
+		echo "Downloading $version.src"
+		mkdir $version.src
+		wget $source_url --quiet -O - | tar $(tar_flags $source_url) --strip-components=1 -C $version.src --occurrence=1 --wildcards '*/docs/ClangFormatStyleOptions.rst'
+	fi
+
+	if [ ! -d $version ]
+	then
+		echo "Downloading $version"
+		mkdir $version
+		wget $binary_url --quiet -O - | tar $(tar_flags $binary_url) --strip-components=1 -C $version --occurrence=1 --wildcards '*bin/clang-format'
+	fi
 done
