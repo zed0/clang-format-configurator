d74f0552f490740b0ae2f929bfe644be24e5fde2
Josh Kelley <joshkel@gmail.com>
2017-05-17 22:51:51 -0400

    Minor updates
    Fix Markdown in README; delete comment about hard-coded URLs, since
    those are properly configurable now.
    
    Add .editorconfig.
    
    Add new Clang versions.
    
    Fix incorrect reference to 3.8.1 versus 3.8.0.

--- server/js/settings.js
+++ server/js/settings.js
@@ -1,5 +1,5 @@
 (function(exports){
-	exports.versions    = ['HEAD', '3.9.0', '3.8.1', '3.8.0', '3.7.0', '3.6.2', '3.6.1', '3.6.0', '3.5.2', '3.5.0', '3.4.2', '3.4.1', '3.4'];
+	exports.versions    = ['HEAD', '4.0.0', '3.9.1', '3.9.0', '3.8.1', '3.8.0', '3.7.0', '3.6.2', '3.6.1', '3.6.0', '3.5.2', '3.5.0', '3.4.2', '3.4.1', '3.4'];
 	exports.port        = 8038;
 	exports.url         = 'https://zed0.co.uk';
 	exports.privKeyPath = './privkey.pem';
