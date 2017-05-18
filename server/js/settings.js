a5250ab548aeb15be275f3c66e45f38394568fe7
Josh Kelley <joshkel@gmail.com>
2017-05-17 22:59:11 -0400

    Server updates and improvements
    Allow running in HTTP during development.
    
    Allow running on a non-standard port.
    
    Fix finding LLVM outside of the server/js directory.

--- server/js/settings.js
+++ server/js/settings.js
@@ -1,6 +1,7 @@
 (function(exports){
 	exports.versions    = ['HEAD', '4.0.0', '3.9.1', '3.9.0', '3.8.1', '3.8.0', '3.7.0', '3.6.2', '3.6.1', '3.6.0', '3.5.2', '3.5.0', '3.4.2', '3.4.1', '3.4'];
 	exports.port        = 8038;
+	exports.clientPort  = undefined;
 	exports.url         = 'https://zed0.co.uk';
 	exports.privKeyPath = './privkey.pem';
 	exports.pubKeyPath  = './fullchain.pem';
